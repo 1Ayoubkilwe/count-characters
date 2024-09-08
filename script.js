29ec854f523ff61edd87bb7cdd31705fdb7041e6
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 11:15:06 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+function count(char, str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === char) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  // Test cases
+  console.log(count("a", "fikrcamp")); // 1
+  console.log(count("c", "bootcamp")); // 1
+  console.log(count("o", "fikrcamp coding bootcamp")); // 3
+  
